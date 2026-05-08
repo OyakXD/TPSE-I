7698c41810936e21e63321518bd89e83c135ea09
OyakXD <kayovieira157@gmail.com>
2026-05-08 12:56:08 -0300

    feat:

---
+++ lab/pratica_02/inc/hw_types.h
@@ -0,0 +1,8 @@
+#ifndef _HW_TYPES_H_
+#define _HW_TYPES_H_
+#include "bbb_regs.h"
+
+#define CM_PER_GPIO1_CLKCTRL_MODULEMODE_ENABLE      0x2
+#define CONF_GPMC_A5                                (*((volatile unsigned int*) 0x44E10854))                             
+#define CONF_GPMC_A5_MODE                           0x7                               
+#endif
--- lab/pratica_02/src/main.c
+++ lab/pratica_02/src/main.c
@@ -1,23 +1,23 @@
 #include "bbb_regs.h"
+#include "hw_types.h"
 #include <stdbool.h>
 
-/* Setup mode, output 0 , input 1*/
+/* Setup mode, output 0 , input 1, reset is 1*/
 #define setupGPIO1(num, key) \
         do { \
             (GPIO1_OE) &= ~((key) << (num)); \
         } while(0)
 
+/* o reset do pinmux é o 7, então para este caso declarar o pinmux dele não seria necessário, mas é uma boa prática. */
+static void configPINMUX(){
+    CONF_GPMC_A5 |= CONF_GPMC_A5_MODE;
+}
 
 /* Init clock for gpio1*/
 static void initClockGPIO1(){
-    CM_PER_GPIO1_CLKCTRL |= 0x2;
+    CM_PER_GPIO1_CLKCTRL |= CM_PER_GPIO1_CLKCTRL_MODULEMODE_ENABLE;
 }
 
-/* 1° prototipo, tornei ele um pré-processo
-static void setupGPIO1(volatile unsigned int num, volatile bool key){
-    GPIO1_OE &=  ~(key << num);
-}
-*/
 
 /* Blink internal leds*/
 static void blink_int(volatile unsigned int num){
@@ -29,13 +29,15 @@ static void blink_int(volatile unsigned int num){
 
 int main(){
     initClockGPIO1();
+    configPINMUX();
 
     setupGPIO1(21, 1);
     setupGPIO1(22, 1);
     setupGPIO1(23, 1);
     setupGPIO1(24, 1);
     
-    while(1){
+    /* Verificação de IDLEST STATUS DO CLOCK DO GPIO1 */
+    while(!((CM_PER_GPIO1_CLKCTRL >> 16) & 0x1) && !((CM_PER_GPIO1_CLKCTRL >> 17) & 0x1)){
 
         blink_int(21);
         blink_int(22);
