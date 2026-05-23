aba8e325fc443f4471003c2efbbf2a2bbcef1393
OyakXD <kayovieira157@gmail.com>
2026-05-23 12:15:43 -0300

    Main refatorada para a nova estrutura

--- lab/pratica_02/src/main.c
+++ lab/pratica_02/src/main.c
@@ -1,22 +1,14 @@
 #include "soc_AM335x.h"
 #include "hw_types.h"
+#include "system.h"
+#include "gpio.h"
 #include <stdbool.h>
 
 /* OFFSETS IN  USAGE */
-#define CM_PER_GPIO_1_CLKCTRL   0xAC
-#define CM_PER_GPIO_2_CLKCTRL   0xB0
-#define CONF_GPMC_CSN3          0x888
 #define CONF_GPMC_CLK           0x88C
 #define CONF_GPMC_A5            0x854
-#define MODULEMODEENABLE        0x2
-#define CONF_GPMC_A5_MODE       0x7
-#define CONF_GPMC_CSN3_MODE     0x7
-#define CONF_GPMC_CLK_MODE      0x7
-#define GPIO_OE                 0x134
 #define GPIO_SETDATAOUT         0x194
-#define GPIO_CLEARDATAOUT       0x190
-#define WDT_WWPS                0x34
-#define WDT_WSPR                0x48
+#define GPIO_CLEARDATAOUT       0x190           
 /* ------------------------------------*/
 
 /* Blink internal leds*/
@@ -32,33 +24,32 @@ static void blink(volatile unsigned int num){
 
 int main(){
 
-    /* Disable WDT */
-    HWREG(SOC_WDT_1_REGS + WDT_WSPR) = 0xAAAA;
-    while(HWREG(SOC_WDT_1_REGS + WDT_WWPS) & (1 << 4));
-    HWREG(SOC_WDT_1_REGS + WDT_WSPR) = 0x5555;
-    while(HWREG(SOC_WDT_1_REGS + WDT_WWPS) & (1 << 4));
+    systemInit();
     
     /* config CLOCKS*/
-    HWREG(SOC_CM_PER_REGS + CM_PER_GPIO_1_CLKCTRL) |= MODULEMODEENABLE;
-    HWREG(SOC_CM_PER_REGS + CM_PER_GPIO_2_CLKCTRL) |= MODULEMODEENABLE;
-    /*-------------------------*/
+    enableClockGPIOx(1);
+    enableClockGPIOx(2);
+    /*-----------------*/
 
     /* set pinmux */
-    HWREG(SOC_CONTROL_REGS + CONF_GPMC_A5) |= CONF_GPMC_A5_MODE;
-    HWREG(SOC_CONTROL_REGS + CONF_GPMC_CLK) |= CONF_GPMC_CLK_MODE;
+    setPinMuxGPIO(CONF_GPMC_A5);
+    setPinMuxGPIO(CONF_GPMC_CLK);
     /*------------------------------------------*/
 
     /* set direction*/
-    HWREG(SOC_GPIO_1_REGS + GPIO_OE) &= ~(1 << 21);
-    HWREG(SOC_GPIO_1_REGS + GPIO_OE) &= ~(1 << 22);
-    HWREG(SOC_GPIO_1_REGS + GPIO_OE) &= ~(1 << 23);
-    HWREG(SOC_GPIO_1_REGS + GPIO_OE) &= ~(1 << 24);
-    HWREG(SOC_GPIO_2_REGS + GPIO_OE) &= ~(1 << 1);
+    setOEDirection(1,21,0);
+    setOEDirection(1,22,0);
+    setOEDirection(1,23,0);
+    setOEDirection(1,24,0);
+    setOEDirection(2,1,0);
     /*----------------------*/
     
+    /* Verificação dos status dos clocks */
+    verifyStatusClockGPIOx(1);
+    verifyStatusClockGPIOx(2);
+    /*------------------------------------ */
     
-    // VERIFICAÇÃO DO STATUS DOS CLOCKS
-    while(((HWREG(SOC_CM_PER_REGS + CM_PER_GPIO_1_CLKCTRL >> 16) & 0x3) != 0x3) && ((HWREG(SOC_CM_PER_REGS + CM_PER_GPIO_2_CLKCTRL >> 16) & 0x3) != 0x3)){
+    while(1){
 
         blink(21);
         blink(22);
